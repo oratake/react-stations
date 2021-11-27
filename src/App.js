fdf18d1115de0a6cfaccafa522ef2a3670a99aae
oratake <smith.shimomura@gmail.com>
2021-11-27 15:50:06 +0900

    feat: station4: useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, {useState} from 'react';
 import './App.css'
 
 /**
@@ -8,12 +8,14 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/bouvier/n02106382_4629.jpg');
+
   return (
     <div>
       <header>header</header>
       <main>
         <h2>Dog picture</h2>
-        <img src="https://dog.ceo/api/breeds/image/random"></img>
+        <img src={dogUrl}></img>
       </main>
     </div>
   )
