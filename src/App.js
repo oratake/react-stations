02b7914a6087101864aba34d743f79811e117924
oratake <smith.shimomura@gmail.com>
2021-11-27 16:21:23 +0900

    feat: station5: ボタン押下で画像が切り替わるように

--- src/App.js
+++ src/App.js
@@ -10,11 +10,18 @@ import './App.css'
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/bouvier/n02106382_4629.jpg');
 
+  const changeDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(json => setDogUrl(json.message));
+  };
+
   return (
     <div>
       <header>header</header>
       <main>
         <h2>Dog picture</h2>
+        <button onClick={changeDog}>Change Dog</button>
         <img src={dogUrl}></img>
       </main>
     </div>
